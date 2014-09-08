20a6eadab0ab86104feb494c97f3a5b3b98042fa
paulo martins <pmotard@sapo.pt>
2014-09-08 22:28:13 +0200

    Background image
    Adicionar uma imagem de fundo ao ecrã inicial

--- SaboresAlentejanosCatalogo/gen/com/example/saboresalentejanoscatalogo/R.java
+++ SaboresAlentejanosCatalogo/gen/com/example/saboresalentejanoscatalogo/R.java
@@ -11,14 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class layout {
         public static final int activity_inicio=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
     }
     public static final class style {
         /** 
