1f2e15939ca7dec398523534cafae88225e43588
paulo martins <pmotard@sapo.pt>
2014-09-09 16:09:52 +0200

    Galeria a funcionar

--- SaboresAlentejanos Catalogo/gen/com/example/saboresalentejanoscatalogo/R.java
+++ SaboresAlentejanos Catalogo/gen/com/example/saboresalentejanoscatalogo/R.java
@@ -8,21 +8,32 @@
 package com.example.saboresalentejanoscatalogo;
 
 public final class R {
+    public static final class array {
+        public static final int image_ids=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int image_1=0x7f020002;
+        public static final int image_2=0x7f020003;
+        public static final int image_3=0x7f020004;
+        public static final int image_4=0x7f020005;
     }
     public static final class id {
-        public static final int bt_entrar=0x7f060000;
+        public static final int bt_entrar=0x7f070001;
+        public static final int gridView=0x7f070000;
+        public static final int image=0x7f070002;
+        public static final int text=0x7f070003;
     }
     public static final class layout {
         public static final int activity_galeria_produtos=0x7f030000;
         public static final int activity_inicio=0x7f030001;
+        public static final int row_grid=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
     }
     public static final class style {
         /** 
@@ -46,10 +57,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- SaboresAlentejanos Catalogo/src/com/example/saboresalentejanoscatalogo/GaleriaProdutos.java
+++ SaboresAlentejanos Catalogo/src/com/example/saboresalentejanoscatalogo/GaleriaProdutos.java
@@ -1,21 +1,46 @@
 package com.example.saboresalentejanoscatalogo;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.GridView;
 import android.widget.Toast;
 
 public class GaleriaProdutos extends Activity {
+	private GridView gridView;
+	private GridViewAdapter customGridAdapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
-		
 		setContentView(R.layout.activity_galeria_produtos);
 		
 		String produto = getIntent().getStringExtra("batata");
 		
-		Toast.makeText(this, produto,Toast.LENGTH_SHORT).show();
+		//Toast.makeText(this, produto,Toast.LENGTH_SHORT).show();
+		
+		gridView = (GridView) findViewById(R.id.gridView);
+		customGridAdapter = new GridViewAdapter(this, R.layout.row_grid,getData());
+		gridView.setAdapter(customGridAdapter);
+	}
+	
+	private ArrayList getData()
+	{
+		final ArrayList imageItems = new ArrayList();
+		
+		TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+		
+		for (int i = 0; i<imgs.length(); i++)
+		{
+			Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), imgs.getResourceId(i, -1));
+			imageItems.add(new ImageItem(bitmap, "Image#" + i));
+		}
+		return imageItems;
 	}
 
 }
