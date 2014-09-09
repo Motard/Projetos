0b033310bb6d1ea77ce163a85cec979cb6bc12f5
paulo martins <pmotard@sapo.pt>
2014-09-09 11:10:31 +0200

    Imagem Inicio

--- SaboresAlentejanos Catalogo/src/com/example/saboresalentejanoscatalogo/Inicio.java
+++ SaboresAlentejanos Catalogo/src/com/example/saboresalentejanoscatalogo/Inicio.java
@@ -20,36 +20,17 @@ public class Inicio extends Activity {
         
         //Aceder ao botão e associar uma acção ao mesmo
         Button launchActivityListaProdutos = (Button) findViewById(R.id.bt_entrar);
-        launchActivityListaProdutos.setOnClickListener(new OnClickListener() {
+        launchActivityListaProdutos.setOnClickListener(new OnClickListener() 
+        {
 			
 			@Override
-			public void onClick(View v) {
+			public void onClick(View v) 
+			{
 				
 				Intent intent = new Intent(Inicio.this, ListaProdutos.class);
 				startActivity(intent);
 				
 			}
 		});
-        
-        
-        
-        
-        
-//        Button launchActivityTwoButton = (Button) findViewById(R.id.bLaunchActivityTwo); 
-//		launchActivityTwoButton.setOnClickListener(new OnClickListener() {
-//			
-//			@Override
-//			public void onClick(View v) {
-//				
-//				// Launch Activity Two
-//				// Hint: use Context's startActivity() method
-//
-//				// Create an intent stating which Activity you would like to start
-//				Intent intent = new Intent(ActivityOne.this, ActivityTwo.class);
-//				
-//				// Launch the Activity using the intent
-//				startActivity(intent);
-//			
-//			}
     }
 }
